ff08578d79930c86202ca6ba2552c24cbf7e9d84
lotcz <mojemejly@centrum.cz>
2013-11-14 19:54:39 +0100

    editor refactored

--- src/mapEditor.js
+++ src/mapEditor.js
@@ -25,7 +25,6 @@ function mapEditor () {
 		this.parameters = {
 			floatUp: function() { editor.skywalker = true;party.position.stepsUp += 1; dr.startMovingParty(); hud.refresh(); },
 			floatDown: function() { editor.skywalker = true;party.position.stepsUp -= 1; dr.startMovingParty(); hud.refresh(); },
-			eraseTiles: function() { editor.eraseTiles() }
 		};
 		
 		var folder0 = this.gui.addFolder('Float');
@@ -33,30 +32,18 @@ function mapEditor () {
 		folder0.add( this.parameters, 'floatUp' ).name("Up");
 		folder0.add( this.parameters, 'floatDown' ).name("Down");
 		folder0.open();
-		
-		var folder1 = this.gui.addFolder('Eraser');
-		folder1.add( this.parameters, 'eraseTiles' ).name("Almighty eraser");
-		folder1.open();
+				
 	}
 	
 	this.mainMenu = function () {
 		this.reset();
 		
-		this.parameters = {
-			addTiles: function() { editor.addTiles() },
-			addMapObject: function() { editor.addMapObject() },
-			selectMapObjects: function() { editor.selectMapObjects() },
-		};
-		
-		var folder1 = this.gui.addFolder('Tiles');
-		folder1.add( this.parameters, 'addTiles' ).name("Dungeon drawing");
-		
+		var folder1 = this.gui.addFolder('Dungeon drawing');
+		folder1.add( this, 'eraseTiles' ).name("Almighty eraser");
+		folder1.add( this, 'addTiles' ).name("Draw tiles");
+		folder1.add( this, 'addMapObject' ).name("Add object");
+		folder1.add( this, 'selectMapObjects' ).name("Select");		
 		folder1.open();
-		
-		var folder2 = this.gui.addFolder('Objects');
-		folder2.add( this.parameters, 'addMapObject' ).name("Object drawing");
-		folder2.add( this.parameters, 'selectMapObjects' ).name("Select");
-		folder2.open();
 		
 		this.gui.open();
 	}
@@ -65,31 +52,24 @@ function mapEditor () {
 	this.addTiles = function () {
 		this.reset();
 				
-		this.parameters = {
-			tileTypeStr: this.defaultTileTypeStr,
-			selectMaterial: function() { editor.selectMaterial() },
-			insertTile: function() { editor.insertTile() },
-			cancel: function() { editor.mainMenu() },
-		};
 		var folder1 = this.gui.addFolder("Dungeon drawing");
-		folder1.add( this.parameters, 'tileTypeStr', [ "Floor", "Ceiling", "Wall" ] ).name('Type');
+		folder1.add( this, 'defaultTileTypeStr', [ "Floor", "Ceiling", "Wall" ] ).name('Type');
 		folder1.add( this, 'defaultMaterialID' ).name('Material').listen();
-		folder1.add( this.parameters, 'selectMaterial' ).name('Select material');
-		folder1.add( this.parameters, 'insertTile' ).name('Save');
-		folder1.add( this.parameters, 'cancel' ).name('Cancel');
+		folder1.add( this, 'selectMaterial' ).name('Select material');
+		folder1.add( this, 'insertTile' ).name('Save');
+		folder1.add( this, 'mainMenu' ).name('Cancel');
 		folder1.open();
 		
 		this.gui.open();
 	}
 	
 	this.insertTile = function () {
-		this.defaultTileTypeStr = this.parameters.tileTypeStr;
 		var tile = new dungeonTile();
 		tile.stepsSouth = party.position.stepsSouth;
 		tile.stepsEast = party.position.stepsEast;
 		tile.stepsUp = party.position.stepsUp;
 		tile.direction = party.position.direction;
-		tile.tileType = getTileTypeFromString(this.parameters.tileTypeStr);
+		tile.tileType = getTileTypeFromString(this.defaultTileTypeStr);
 		tile.materialID = this.defaultMaterialID;
 		tile.addToScene(dr.scene, dr.tileGeometry, map.materials);
 		$.post("ghobok.php", { method: "save_tile", map_id:map.mapID, tile_json:tile.getJSON() }, function (data) { console.log("Tile inserted:" + data); } );
@@ -128,8 +108,7 @@ function mapEditor () {
 	/* MAP OBJECTS */
 	this.addMapObject = function () {
 		this.reset();
-		this.map_object = new mapObject();
-		this.map_object.position = party.position.clone();
+		this.map_object = new mapObject();		
 		this.openMapObjectEditor();
 	}
 	
@@ -142,26 +121,20 @@ function mapEditor () {
 	
 	this.openMapObjectEditor = function() {
 	
-		this.parameters = {
-			selectObject: function() { editor.selectObject() },
-			insertMapObject: function() { editor.insertMapObject() },
-			cancel: function() { editor.mainMenu() },
-		};
-		
 		var folder1 = this.gui.addFolder("Object drawing");
 		folder1.add( this.map_object, 'objectID').name('Object ID').listen();
-		folder1.add( this.parameters, 'selectObject' ).name('Select object');
+		folder1.add( this, 'selectObject' ).name('Select object');
 		
 		folder1.add( this.map_object, 'positionX' ).name('X').min(-500).max(500).step(1).listen().onChange(function(value) { editor.updateEditedMapObject(); });
-		folder1.add( this.map_object, 'positionY' ).name('Y').min(-500).max(500).step(1).listen().onChange(function(value) {	editor.updateEditedMapObject(); });
-		folder1.add( this.map_object, 'positionZ' ).name('Z').min(-500).max(500).step(1).listen().onChange(function(value) {	editor.updateEditedMapObject(); });
+		folder1.add( this.map_object, 'positionY' ).name('Y').min(-500).max(500).step(1).listen().onChange(function(value) { editor.updateEditedMapObject(); });
+		folder1.add( this.map_object, 'positionZ' ).name('Z').min(-500).max(500).step(1).listen().onChange(function(value) { editor.updateEditedMapObject(); });
 		folder1.add( this.map_object, 'rotationX' ).name('Rotation X').min(- DOUBLE_RIGHT_ANGLE).max(DOUBLE_RIGHT_ANGLE).step(0.01).listen().onChange(function(value) { editor.updateEditedMapObject(); });
 		folder1.add( this.map_object, 'rotationY' ).name('Rotation Y').min(- DOUBLE_RIGHT_ANGLE).max(DOUBLE_RIGHT_ANGLE).step(0.01).listen().onChange(function(value) { editor.updateEditedMapObject(); });
 		folder1.add( this.map_object, 'rotationZ' ).name('Rotation Z').min(- DOUBLE_RIGHT_ANGLE).max(DOUBLE_RIGHT_ANGLE).step(0.01).listen().onChange(function(value) { editor.updateEditedMapObject(); });
 		folder1.add( this.map_object, 'scale' ).name('Scale').min(0.01).max(15).step(0.01).listen().onChange(function(value) { editor.updateEditedMapObject(); });
 		
-		folder1.add( this.parameters, 'insertMapObject' ).name('Save');
-		folder1.add( this.parameters, 'cancel' ).name('Cancel');
+		folder1.add( this, 'saveMapObject' ).name('Save');
+		folder1.add( this, 'mainMenu' ).name('Cancel');
 		folder1.open();
 		
 		this.gui.open();
@@ -175,7 +148,7 @@ function mapEditor () {
 		}
 	}
 	
-	this.insertMapObject = function () {
+	this.saveMapObject = function () {
 		if (!this.map_object.mesh) {
 			this.map_object.addToScene(dr.scene, map.objects, map.materials);
 		} else {
@@ -188,6 +161,7 @@ function mapEditor () {
 			}
 			console.log("Save map object:" + data); 
 		} );
+		this.mainMenu();
 	}
 	
 	this.openObjectManager = function() {
@@ -232,6 +206,8 @@ function mapEditor () {
 					$(e).click( function () {
 						var object_id = $(e).attr('objectID');						
 						editor.map_object.objectID = object_id;
+						editor.map_object.position = party.position.clone();
+						editor.map_object.position.forward();						
 						var object_json = JSON.parse($(".object_json",$(e).parent()).html());
 						editor.map_object.positionX = object_json.default_position_x;
 						editor.map_object.positionY = object_json.default_position_y;
@@ -240,6 +216,7 @@ function mapEditor () {
 						editor.map_object.rotationY = object_json.default_rotation_y;
 						editor.map_object.rotationZ = object_json.default_rotation_z;
 						editor.map_object.scale = object_json.default_scale;
+						editor.map_object.addToScene(dr.scene, map.objects, map.materials);
 						editor.closeObjectManager();
 					});
 				} 
